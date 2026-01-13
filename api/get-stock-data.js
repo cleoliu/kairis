ce96e283e45a1a5dd088cb0dcfda21ea4342a07c
Cleo Liu <171683151+CleoliuGo@users.noreply.github.com>
2026-01-13 23:11:58 +0800

    Revert "Update get-stock-data.js"
    This reverts commit b24ac40170baf76730a2dcaa1cb322de7dc43840.

--- api/get-stock-data.js
+++ api/get-stock-data.js
@@ -17,7 +17,7 @@ const polygonRateLimit = {
   requestTimestamps: [],
   maxRequests: 5,
   windowMs: 60000, // 1分鐘
-  minInterval: 1000, // 每次請求間隔1秒（從12秒降低）
+  minInterval: 12000, // 每次請求間隔12秒
   isRateLimited: false,
   rateLimitResetTime: 0
 };
